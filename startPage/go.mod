ccdf3c0ce302d64b7b0987c3e3110003b741e59f
NikhitaBoyidapu <nboyidap@uncc.edu>
2024-04-12 19:01:52 -0400

    startpage with username

--- startPage/go.mod
+++ startPage/go.mod
@@ -4,13 +4,9 @@ go 1.22.2
 
 replace gamePage => ../gamePage
 
-require github.com/hajimehoshi/ebiten v1.12.12
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20240408130534-53f26d8a0802
 
 require (
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.15.0 // indirect
-	golang.org/x/mobile v0.0.0-20210208171126-f462b3930c8f // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/ebitengine/purego v0.6.0-alpha.1.0.20231122024802-192c5e846faa // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
--- startPage/startPage.go
+++ startPage/startPage.go
@@ -1,88 +1,145 @@
-// working startPage
 package main
 
 import (
-	"image/color"
-	"log"
-	"os"
 	"os/exec"
 
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
-	"github.com/hajimehoshi/ebiten/inpututil"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-const (
-	screenWidth   = 640
-	screenHeight  = 480
-	buttonWidth   = 200
-	buttonHeight  = 60
-	buttonPadding = 20
-)
-
-var (
-	iconImage   *ebiten.Image
-	buttonX     = (screenWidth - buttonWidth) / 2
-	buttonY     = (screenHeight - buttonHeight) / 2
-	imageWidth  = 200 // Adjust as per requirement
-	imageHeight = 200 // Adjust as per requirement
-)
-
-func update(screen *ebiten.Image) error {
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-		mouseX, mouseY := ebiten.CursorPosition()
-		if mouseX >= buttonX && mouseX <= buttonX+buttonWidth && mouseY >= buttonY && mouseY <= buttonY+buttonHeight {
-			// Handle button click
-			log.Println("Button clicked!")
-			err := exec.Command("go", "run", "../gamePage/gamePage.go").Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-			os.Exit(0) // Exit after opening gamePage.go
-		}
-	}
-
-	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
-		// Handle enter key press
-	}
-
-	// Clear the screen
-	screen.Fill(color.White)
-
-	// Draw the icon image
-	if iconImage != nil {
-		opts := &ebiten.DrawImageOptions{}
-		opts.GeoM.Translate(float64(screenWidth/2-imageWidth/2), float64(screenHeight/4-imageHeight/2))
-		screen.DrawImage(iconImage, opts)
-	}
-
-	// Draw the "New Game" button
-	drawButton(screen, buttonX, buttonY, "New Game")
-
-	return nil
+// TextInput represents a text input box.
+type TextInput struct {
+	rect       rl.Rectangle
+	text       string
+	active     bool
+	fontSize   int32
+	fontColor  rl.Color
+	normalText string // Text to display when not active
+	label      string // Label for the text input box
 }
 
-func drawButton(screen *ebiten.Image, x, y int, text string) {
-	// Draw the button background
-	ebitenutil.DrawRect(screen, float64(x), float64(y), float64(buttonWidth), float64(buttonHeight), color.RGBA{100, 100, 100, 255})
+// NewTextInput creates a new text input box.
+func NewTextInput(rect rl.Rectangle, label string, normalText string, fontSize int32, fontColor rl.Color) TextInput {
+	return TextInput{
+		rect:       rect,
+		label:      label,
+		normalText: normalText,
+		fontSize:   fontSize,
+		fontColor:  fontColor,
+	}
+}
 
-	// Draw the text inside the button
-	textWidth := ebitenutil.MeasureText(text)
-	textX := x + (buttonWidth-textWidth)/2
-	textY := y + (buttonHeight-ebitenutil.DefaultFont.Metrics().Height.Ceil())/2
-	ebitenutil.DebugPrintAt(screen, text, textX, textY)
+// Draw draws the text input box.
+func (t *TextInput) Draw() {
+	// Draw label
+	rl.DrawText(t.label, int32(t.rect.X), int32(t.rect.Y-25), t.fontSize, t.fontColor)
+
+	if t.active {
+		rl.DrawRectangleRec(t.rect, rl.White) // Background color
+		rl.DrawRectangleLinesEx(t.rect, 1, rl.Black)
+		rl.DrawText(t.text, int32(t.rect.X+5), int32(t.rect.Y+10), t.fontSize, t.fontColor)
+	} else {
+		rl.DrawRectangleRec(t.rect, rl.LightGray)
+		rl.DrawRectangleLinesEx(t.rect, 1, rl.Gray)
+		rl.DrawText(t.normalText, int32(t.rect.X+5), int32(t.rect.Y+10), t.fontSize, rl.Gray)
+	}
+}
+
+// Update handles text input for the text input box.
+func (t *TextInput) Update() {
+	if rl.CheckCollisionPointRec(rl.GetMousePosition(), t.rect) && rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+		t.active = true
+	} else if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+		t.active = false
+	}
+
+	if t.active {
+		key := rl.GetKeyPressed()
+		if key >= 32 && key <= 125 && len(t.text) < 16 {
+			t.text += string(key)
+		} else if key == rl.KeyBackspace && len(t.text) > 0 {
+			t.text = t.text[:len(t.text)-1]
+		}
+	}
 }
 
 func main() {
-	// Load the icon image
-	var err error
-	iconImage, _, err = ebitenutil.NewImageFromFile("Icon.png", ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
+	screenWidth := int32(800)
+	screenHeight := int32(600)
+
+	rl.InitWindow(screenWidth, screenHeight, "Window with Texture and Button")
+
+	// Load texture
+	texture := rl.LoadTexture("Icon.png")
+	if texture.ID == 0 {
+		rl.TraceLog(rl.LogError, "Failed to load texture")
+		return
 	}
 
-	// Create a new game window
-	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "Start Page"); err != nil {
-		log.Fatal(err)
+	// Padding for the texture from the left edge
+	padding := int32(50)
+
+	// Button position and size
+	buttonWidth := int32(200)
+	buttonHeight := int32(50)
+	buttonX := screenWidth - buttonWidth - 50 // 50 pixels padding from the right edge
+	buttonY := screenHeight/2 - buttonHeight/2
+
+	// Padding between button and text box
+	paddingY := int32(20)
+
+	// Username text box
+	usernameBox := NewTextInput(rl.NewRectangle(float32(buttonX), float32(buttonY-buttonHeight-paddingY), float32(buttonWidth), float32(buttonHeight)), "Username:", "Enter your username", 15, rl.Black)
+
+	// Main loop
+	for !rl.WindowShouldClose() {
+		// Update
+
+		// Check if the button is clicked
+		mousePosition := rl.GetMousePosition()
+		if rl.CheckCollisionPointRec(mousePosition, rl.NewRectangle(float32(buttonX), float32(buttonY), float32(buttonWidth), float32(buttonHeight))) && rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			// Open the gamePage.go file when the button is clicked, pass the username as an argument
+			if usernameBox.text != "" {
+				cmd := exec.Command("go", "run", "../gamePage/gamePage.go", usernameBox.text)
+				err := cmd.Run()
+				if err != nil {
+					rl.TraceLog(rl.LogError, "Failed to open gamePage.go:", err)
+				}
+			} else {
+				// Username not entered, display a message to the user or take appropriate action
+				rl.TraceLog(rl.LogInfo, "Please enter a username.")
+			}
+		}
+
+		// Update username text box
+		usernameBox.Update()
+
+		// Draw
+		rl.BeginDrawing()
+
+		rl.ClearBackground(getBiscuitColor())
+
+		// Draw texture with left padding
+		textureX := padding
+		textureY := screenHeight/2 - texture.Height/2
+		rl.DrawTexture(texture, textureX, textureY, rl.White)
+
+		// Draw button on the right half
+		rl.DrawRectangle(buttonX, buttonY, buttonWidth, buttonHeight, rl.Green)
+		rl.DrawText("New Game", buttonX+25, buttonY+15, 20, rl.White)
+
+		// Draw username text box
+		usernameBox.Draw()
+
+		rl.EndDrawing()
 	}
+
+	// Unload texture
+	rl.UnloadTexture(texture)
+
+	rl.CloseWindow()
+}
+
+// getBiscuitColor returns the "biscuit" color (RGB: 255, 228, 196).
+func getBiscuitColor() rl.Color {
+	return rl.NewColor(255, 228, 196, 255)
 }
